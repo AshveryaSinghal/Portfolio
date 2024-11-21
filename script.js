83a21f4acd091443c607ec3ec152a2d6ece3bc13
Ashverya Singhal <ashveryasinghal@gmail.com>
2024-11-21 15:42:28 +0530

    Initial commit

--- script.js
+++ script.js
@@ -7,20 +7,18 @@ function typeEffect() {
     if (index < myName.length) {
         nameElement.innerHTML += myName.charAt(index);
         index++;
-        setTimeout(typeEffect, 150); // Adjust typing speed (milliseconds)
+        setTimeout(typeEffect, 150); 
     } else {
-        // Reset the index to restart the typing effect
         index = 0;
-        nameElement.innerHTML = ''; // Clear the content before restarting
-        setTimeout(typeEffect, 1000); // Wait for a second before restarting
+        nameElement.innerHTML = ''; 
+        setTimeout(typeEffect, 1000); 
     }
 }
 
-// Start the typing effect when the page loads
+
 window.onload = function() {
     typeEffect();
 };
 function openProject(url) {
-    // Add any logic to handle opening the project page (e.g., redirecting)
     window.location.href = url;
 }
